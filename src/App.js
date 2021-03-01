d76eef82014fddc59f892b4690709d9f95d8b75a
nedra <nedra.chahed@gmail.com>
2021-03-01 03:09:51 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import UserList from './components/UserList/UserList';
+import Footer from './components/Footer/Footer';
+function App() {
+  return (
+    <div>
+    <div className="App">
+ <UserList/>
+ </div>
+ <div> <Footer/></div>
+
+  
+    </div>
+  );
+}
+
+export default App;
